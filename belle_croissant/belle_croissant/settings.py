9f1f4fdbd56050f727a2976154521c19f3fa0a95
Venius24 <WonderRebirth@mail.ru>
2026-02-11 10:34:42 +0500

    db changes

--- belle_croissant/belle_croissant/settings.py
+++ belle_croissant/belle_croissant/settings.py
@@ -129,6 +129,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
+
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated', 
+    ],
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
